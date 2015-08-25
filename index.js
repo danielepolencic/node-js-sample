cea955751c90098798aedd3a28329a78a6c3470b
Michael Hobbs <michaelshanehobbs@gmail.com>
2015-08-25 09:00:13 -0700

    process handling

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-var express = require('express')
+var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+  response.send('Hello World!');
+});
+
+process.on( "SIGINT", function() {
+  console.log('CLOSING [SIGINT]');
+  process.exit();
+} );
+
+process.on( "SIGTERM", function() {
+  console.log('CLOSING [SIGTERM]');
+  process.exit();
+} );
+
+process.on( "SIGKILL", function() {
+  console.log('CLOSING [SIGKILL]');
+  process.exit();
+} );
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
